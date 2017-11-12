539c0ea8a373f4aed92fc4299614d93f52a28693
David Driscoll <david.driscoll@gmail.com>
2017-11-12 13:21:46 -0500

    Added more logging unit tests

--- test/Logging.Tests/LoggingBuilderTests.cs
+++ test/Logging.Tests/LoggingBuilderTests.cs
@@ -60,4 +60,64 @@ namespace Rocket.Surgery.Extensions.Logging.Tests
             a.Should().NotThrow();
         }
     }
+
+    public class LoggingExtensionTests
+    {
+        [Fact]
+        public void TimeInformationShouldNotBeNull()
+        {
+            A.Fake<ILogger>().TimeInformation("message").Should().NotBeNull();
+        }
+
+        [Fact]
+        public void TimeInformationShouldDispose()
+        {
+            Action a = () =>
+            {
+                using (A.Fake<ILogger>().TimeInformation("message"))
+                {
+
+                }
+            };
+            a.Should().NotThrow();
+        }
+
+        [Fact]
+        public void TimeDebugShouldNotBeNull()
+        {
+            A.Fake<ILogger>().TimeDebug("message").Should().NotBeNull();
+        }
+
+        [Fact]
+        public void TimeDebugShouldDispose()
+        {
+            Action a = () =>
+            {
+                using (A.Fake<ILogger>().TimeDebug("message"))
+                {
+
+                }
+            };
+            a.Should().NotThrow();
+        }
+
+        [Fact]
+        public void TimeTraceShouldNotBeNull()
+        {
+            A.Fake<ILogger>().TimeTrace("message").Should().NotBeNull();
+        }
+
+        [Fact]
+        public void TimeTraceShouldDispose()
+        {
+            Action a = () =>
+            {
+                using (A.Fake<ILogger>().TimeTrace("message"))
+                {
+
+                }
+            };
+            a.Should().NotThrow();
+        }
+    }
 }
